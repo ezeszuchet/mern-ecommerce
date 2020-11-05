c87ba9bc8854ae13efe15921821640a27fec05b4
ezeszuchet <szuchetezequiel@gmail.com>
2020-11-05 17:44:17 -0300

    Login, admin completed

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 //routes
-const userRoutes = require('./routes/user')
+const authRoutes = require('./routes/auth')
+const adminRoutes = require('./routes/admin/auth')
 
 //environment variable
 env.config();
@@ -25,7 +26,8 @@ mongoose.connect(
 
 
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use('/api', authRoutes);
+app.use('/api', adminRoutes);
 
 app.use(bodyParser());
 
