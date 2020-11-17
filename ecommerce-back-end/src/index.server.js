052c4686a284b39b79b2218b4e36b3e2cecc9056
ezeszuchet <szuchetezequiel@gmail.com>
2020-11-17 20:07:38 -0300

    Add to cart

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 //routes
 const authRoutes = require('./routes/auth')
 const adminRoutes = require('./routes/admin/auth')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
+const cartRoutes = require('./routes/cart')
 
 //environment variable
 env.config();
@@ -27,10 +29,12 @@ mongoose.connect(
 
 
 app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
